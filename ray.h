19b304856115d397b548ee64dcda22704a47237d
bjeker <rbieke1@gmail.com>
2024-08-04 01:56:51 -0500

    updated ray comments

--- ray.h
+++ ray.h
@@ -10,6 +10,7 @@ class ray {
         //create a ray using an origin point and a direction vector
         ray(const point3& origin, const vec3& direction) : orig(origin), dir(direction) {}
 
+        //return an immutable reference to members, either use reference directly or make mutable copy
         const point3& origin() const { return orig; }
         const vec3& direc() const { return dir; }
 
